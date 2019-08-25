b48c0a08583754cef6c348181ff094894ea3ef52
Oliver Smith <ollieparanoid@postmarketos.org>
2019-08-25 22:23:10 +0200

    Add {diff_repo,restore_from}_snapshot.sh

---
+++ restore_from_snapshot.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Copyright 2019 Oliver Smith
+#
+# This file is part of pmOS-repository-scripts.
+#
+# pmOS-repository-scripts is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pmOS-repository-scripts is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pmOS-repository-scripts.  If not, see <http://www.gnu.org/licenses/>.
+
+# Script location
+DIR="$(cd $(dirname $0); pwd -P)"
+cd "$DIR"
+
+PACKAGES="$DIR/data/work/packages"
+SNAPSHOT="$DIR/data/packages_snapshot"
+
+
+sudo rsync \
+	--links \
+	--info=progress2 \
+	--human-readable \
+	--recursive \
+	--size-only \
+	--delete \
+	"$SNAPSHOT"/* \
+	"$PACKAGES"
+sudo chown -R 12345:12345 "$PACKAGES"
+pmbootstrap index
